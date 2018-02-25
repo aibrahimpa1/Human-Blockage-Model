b6533df8d5996ebb77cc8f92aa9f0713f7dfddef
aibrahimpa1 <aibrahimpa1@etf.unsa.ba>
2018-02-25 23:26:12 +0100

    Different scenarios for different number of pedestrians

--- Simulation.m
+++ Simulation.m
@@ -18,36 +18,62 @@ Ha = 3;
 % create antenna object
 antenna = Antenna(Xa, Ya, Ha);
 
-[prob, history] = Motion_simulation(W,L,N_pedestrians(1), N_steps, antenna);
+Probablities = {};
+Histories = {};
 
+for i=1:3
+    [Probablities{i}, Histories{i}] = Motion_simulation(W,L,N_pedestrians(i), N_steps, antenna);
 
-% Plot movement of pedestrians
-PlotHistory(history, W, L, N_steps, N_pedestrians);
+    % Plot movement of pedestrians
+    PlotHistory(Histories{i}, W, L, N_steps, N_pedestrians(i));
+
+    % Transform the probablity distribution
+    [distances, distprob] = ProbXY2R(antenna, Probablities{i}, W, L);
+
+    % Calculate attenuation
+    Attenuation = abs(1 - distprob);
+
+    % Log of attentuation for estimating alpha
+    LogAtt = log(Attenuation);
+
+    % Estimating alpha
+    %alpha = polyfit(distances, LogAtt, 1);
+    alpha(i) = (distances.')\(LogAtt.');
+end
 
 % Plot the probablity distribution
 figure, clf
+subplot(2,2,1)
 [X, Y] = meshgrid(1:W,1:L);
-h = surf(X, Y, prob);
+h = surf(X, Y, Probablities{1});
+set(h,'LineStyle','none')
+subplot(2,2,2)
+h = surf(X, Y, Probablities{2});
+set(h,'LineStyle','none')
+subplot(2,2,3)
+h = surf(X, Y, Probablities{3});
 set(h,'LineStyle','none')
 
-% Transform the probablity distribution
-[distances, distprob] = ProbXY2R(antenna, prob, W, L);
-
-% Calculate attenuation
-Attenuation = 1 - distprob;
-
-% Log of attentuation for estimating alpha
-LogAtt = log(Attenuation);
-
-% Estimating alpha
-%alpha = polyfit(distances, LogAtt, 1);
-alpha = (distances.')\(LogAtt.');
-
 % Estimating Attenuation using the estimated alpha value
-AttEstimate = exp(alpha*distances);
-
 figure, clf
+subplot(2,2,1)
+AttEstimate = exp(alpha(1)*distances);
 plot(distances, AttEstimate)
 grid on
 xlabel('Distance to Antenna')
 ylabel('Attenuation')
+title('Silent Case')
+subplot(2,2,2)
+AttEstimate = exp(alpha(2)*distances);
+plot(distances, AttEstimate)
+grid on
+xlabel('Distance to Antenna')
+ylabel('Attenuation')
+title('Busy Case')
+subplot(2,2,3)
+AttEstimate = exp(alpha(3)*distances);
+plot(distances, AttEstimate)
+grid on
+xlabel('Distance to Antenna')
+ylabel('Attenuation')
+title('Crowded Case')
